f8f6fc5c4b4e0e4239b72e0f702a42bc67f67d2f
Alexander Comerford <comerford@new-host-2.home>
2018-10-02 16:25:30 -0400

    Update deploy script build and push into 2 sepearte steps

--- bin/shell.sh
+++ bin/shell.sh
@@ -1,16 +1,12 @@
 IMAGE=cmrfrd.site
-SSHKEY=$1
 docker run \
        -v $(pwd):/site:z \
        -v ~/.ssh:/.ssh:z \
        --rm \
        --name deploy \
        -w /site \
-       -e SSHKEY=$SSHKEY \
        -it cmrfrd.site \
        '
        eval `ssh-agent -s`;
-       ssh-add $SSHKEY;
-       gatsby build;
-       gh-pages -b master -d public -u alexanderjcomerford@gmail.com;
+       sh
        '
