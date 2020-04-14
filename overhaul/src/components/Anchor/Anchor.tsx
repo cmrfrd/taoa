c9a6753a4fb363aac7b0ca3c111410417d2d2228
cmrfrd <alexanderjcomerford@gmail.com>
2020-04-14 01:38:12 -0400

    indents

--- overhaul/src/components/Anchor/Anchor.tsx
+++ overhaul/src/components/Anchor/Anchor.tsx
@@ -1,18 +1,18 @@
 import styled from "@emotion/styled";
 
 const Anchor = styled.a`
-  transition: ${p => p.theme.colorModeTransition};
-  color: ${p => p.theme.colors.accent};
+    transition: ${p => p.theme.colorModeTransition};
+    color: ${p => p.theme.colors.accent};
 
-  &:visited {
+    &:visited {
     color: ${p => p.theme.colors.accent};
     opacity: 0.85;
-  }
+    }
 
-  &:hover,
-  &:focus {
+    &:hover,
+    &:focus {
     text-decoration: underline;
-  }
+    }
 `;
 
 export default Anchor;
