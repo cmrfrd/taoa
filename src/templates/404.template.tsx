a26417d7fa23087c31cb3134a26bec77c63cd84b
cmrfrd <alexanderjcomerford@gmail.com>
2020-05-10 02:38:02 -0400

    linting

--- src/templates/404.template.tsx
+++ src/templates/404.template.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
+import Layout from '@components/Layout';
+import Paragraph from '@components/Paragraph';
+import Section from '@components/Section';
+import { mediaquery } from '@styles/media';
+import { Template, ITAOAThemeUIContext } from '@types';
+
+import styled from '@emotion/styled';
 import { graphql, useStaticQuery } from 'gatsby';
 import _ from 'lodash';
-
+import React from 'react';
 import { Twemoji } from 'react-emoji-render';
-
-import { css } from '@emotion/core';
-import styled from '@emotion/styled';
-
-import Section from '@components/Section';
-import SEO from '@components/SEO';
-import Layout from '@components/Layout';
-import Headings from '@components/Headings';
-import Name from '@components/Name';
-import Paragraph from '@components/Paragraph';
-
-import { Template } from '@types';
-
-import mediaqueries, { mediaquery } from '@styles/media';
-
-import AuthorHero from '../sections/author/Author.Hero';
-import AuthorArticles from '../sections/author/Author.Articles';
 
 const siteQuery = graphql`
   {
@@ -40,7 +29,7 @@ const siteQuery = graphql`
   }
 `;
 
-const Page404: Template = ({ location, pageContext }) => {
+const Page404: Template = ({ location }: Template) => {
   const results = useStaticQuery(siteQuery);
   const { fourOfour } = results.allSite.edges[0].node.siteMetadata;
   const message = _.sample(fourOfour.messages);
@@ -65,7 +54,7 @@ const Page404: Template = ({ location, pageContext }) => {
 
 export default Page404;
 
-const Container = styled('div')((p) => ({
+const Container = styled('div')({
   position: 'relative',
   textAlign: 'center',
   paddingTop: '200px',
@@ -82,9 +71,9 @@ const Container = styled('div')((p) => ({
     paddingTop: '150px',
     paddingBottom: '100px'
   }
-}));
+});
 
-const CenterRowMessage = styled('div')((p) => ({
+const CenterRowMessage = styled('div')({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'center',
@@ -100,71 +89,71 @@ const CenterRowMessage = styled('div')((p) => ({
   [mediaquery.phone()]: {
     margin: '0 20px'
   }
-}));
+});
 
-const Big = styled(Paragraph)`
-  width: auto;
-  font-size: 140px;
-  margin-bottom: 0px;
-  margin: 0 0px 0px;
+const Big = styled(Paragraph)({
+  width: 'auto',
+  fontSize: '140px',
+  marginBottom: '0px',
+  margin: '0 0px 0px',
   [mediaquery.desktop()]: {
-    font-size: 140px;
-    margin-bottom: 0px;
+    fontSize: '140px',
+    marginBottom: '0px'
+  },
+  [mediaquery.tablet()]: {
+    fontSize: '110px',
+    marginBottom: '0px'
+  },
+  [mediaquery.phablet()]: {
+    fontSize: '90px',
+    padding: '0 0px'
+  },
+  [mediaquery.phone()]: {
+    fontSize: '70px',
+    margin: '0 0px 0px'
   }
-  ${mediaquery.tablet()} {
-    font-size: 110px;
-    margin-bottom: 0px;
-  }
-  ${mediaquery.phablet()} {
-    font-size: 90px;
-    padding: 0 0px;
-  }
-  ${mediaquery.phone()} {
-    font-size: 70px;
-    margin: 0 0px 0px;
-  }
-`;
+});
 
-const EmojiContainer = styled.div`
-  padding-left: 30px;
-  font-size: 110px;
-  ${mediaquery.tablet()} {
-    padding-left: 20px;
-    font-size: 120px;
+const EmojiContainer = styled.div({
+  paddingLeft: '30px',
+  fontSize: '110px',
+  [mediaquery.tablet()]: {
+    paddingLeft: '20px',
+    fontSize: '120px'
+  },
+  [mediaquery.phablet()]: {
+    fontSize: '100px'
+  },
+  [mediaquery.phone()]: {
+    fontSize: '80px'
   }
-  ${mediaquery.phablet()} {
-    font-size: 100px;
-  }
-  ${mediaquery.phone()} {
-    font-size: 80px;
-  }
-`;
+});
 
-const Small = styled(Paragraph)`
-  z-index: 1;
-  font-size: 30px;
-  ${mediaquery.desktop()} {
-    margin: 0 auto 10px;
+const Small = styled(Paragraph)({
+  zIndex: 1,
+  fontSize: '30px',
+  [mediaquery.desktop()]: {
+    margin: '0 auto 10px'
+  },
+  [mediaquery.tablet()]: {
+    fontSize: '30px'
+  },
+  [mediaquery.phablet()]: {
+    fontSize: '28px'
+  },
+  [mediaquery.phone()]: {
+    fontSize: '26px'
   }
-  ${mediaquery.tablet()} {
-    font-size: 30px;
-  }
-  ${mediaquery.phablet()} {
-    font-size: 28px;
-  }
-  ${mediaquery.phone()} {
-    font-size: 26px;
-  }
-`;
+});
 
-const AboutGradient = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 390px;
-  z-index: 0;
-  pointer-events: none;
-  background: ${(p) => p.theme.colors.gradient};
-  transition: ${(p) => p.theme.colorModeTransition};
-`;
+const AboutGradient = styled.div((p: ITAOAThemeUIContext) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  height: '390px',
+  zIndex: 0,
+  pointerEvents: 'none',
+  background: `${p.theme.colors.gradient}`,
+  transition: p.theme.colorModeTransition
+}));
