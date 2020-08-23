bc7325d54a3c73b373d2eb76062b20e89be4a1e6
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 11:38:43 -0400

    add loadable to babel config

--- src/gatsby/node/onCreateWebpackConfig.js
+++ src/gatsby/node/onCreateWebpackConfig.js
@@ -1,16 +1,21 @@
+/* eslint-disable */
+
 const path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// const DynamicImportPlugin = require('babel-plugin-dynamic-import-node');
 
 module.exports = ({ actions }) => {
   actions.setWebpackConfig({
+    plugins: [new MonacoWebpackPlugin()],
     resolve: {
       alias: {
         '@components': path.resolve(__dirname, '../../components/'),
         '@icons': path.resolve(__dirname, '../../icons/'),
         '@styles': path.resolve(__dirname, '../../styles/'),
         '@utils': path.resolve(__dirname, '../../utils/'),
-        '@types': path.resolve(__dirname, '../../types/'),
+        '@types': path.resolve(__dirname, '../../types/')
       },
-      extensions: ['.js', '.json', '.ts', '.tsx'],
-    },
+      extensions: ['.js', '.json', '.ts', '.tsx']
+    }
   });
 };
