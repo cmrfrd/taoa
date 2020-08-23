259cb92d35bf709354ec4311dceae7db528fd975
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 11:39:22 -0400

    update scrolling on navigation to go to top of page

--- src/gatsby/browser/shouldUpdateScroll.js
+++ src/gatsby/browser/shouldUpdateScroll.js
@@ -1,22 +1,8 @@
 /* eslint-disable */
 
 module.exports = ({ routerProps, prevRouterProps, getSavedScrollPosition }) => {
-  const currentPosition = getSavedScrollPosition(routerProps.location);
-  const topOfPage = [0, 0];
-
-  if (routerProps.location.action === 'POP' && currentPosition) {
-    window.scrollTo(...currentPosition);
-  } else {
-    window.scrollTo(...topOfPage);
-  }
-
-  // Handling previous path into local storage for "Back" arrow button
-  if (prevRouterProps) {
-    window.localStorage.setItem(
-      'previousPath',
-      prevRouterProps.location.pathname,
-    );
-  }
-
-  return false;
+    const currentPosition = getSavedScrollPosition(routerProps.location,
+                                                   routerProps.location.key);
+    window.scrollTo(...(currentPosition || [0, 0]));
+    return false;
 };
