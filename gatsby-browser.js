044e9ddfbf6a81c81d62a94ef8f25523020a2b82
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-22 23:40:04 -0400

    add on client entry for babel config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@ exports.onInitialClientRender = require('./src/gatsby/browser/onInitialClientRen
 exports.onRouteUpdate = require('./src/gatsby/browser/onRouteUpdate');
 exports.shouldUpdateScroll = require('./src/gatsby/browser/shouldUpdateScroll');
 exports.wrapPageElement = require('./src/gatsby/browser/wrapPageElement').wrapPageElement;
+exports.onClientEntry = require('./src/gatsby/browser/onClientEntry');
