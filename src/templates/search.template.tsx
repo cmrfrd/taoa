586cfe612f4994981da79ceeab9f15378b19132f
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 23:37:33 -0400

    lint search template and type it

--- src/templates/search.template.tsx
+++ src/templates/search.template.tsx
@@ -6,12 +6,14 @@ import Paginator from '@components/Navigation/Navigation.Paginator';
 import SEO from '@components/SEO';
 import Search from '@components/Search';
 import Section from '@components/Section';
-import mediaqueries from '@styles/media';
-import { Template } from '@types';
+import mediaqueries, { mediaquery } from '@styles/media';
+import { Template, TTemplate, IArticle, ITAOAThemeUIContext } from '@types';
 
 import styled from '@emotion/styled';
 import { graphql, useStaticQuery } from 'gatsby';
 import React, { useState } from 'react';
+
+import * as CSS from 'csstype';
 
 const siteQuery = graphql`
   {
@@ -30,18 +32,18 @@ const siteQuery = graphql`
   }
 `;
 
-const SearchPage: Template = ({ location, pageContext }) => {
-  const articles = pageContext.group;
+const SearchPage: Template = ({ location, pageContext }: TTemplate) => {
+  const { articles } = pageContext;
 
   const results = useStaticQuery(siteQuery);
   const { search } = results.allSite.edges[0].node.siteMetadata;
 
   const [searchResults, setSearchResults] = useState(articles);
 
-  const filter = (e, term) => {
+  const filter = (e: IArticle, term: string): boolean => {
     return e.title.toLowerCase().includes(term) || e.title.includes(term);
   };
-  const sort = (a, b) => {
+  const sort = (a: IArticle, b: IArticle): number => {
     if (a.dateForSEO > b.dateForSEO) {
       return -1;
     }
@@ -52,7 +54,7 @@ const SearchPage: Template = ({ location, pageContext }) => {
   };
 
   return (
-    <Layout location={location} enableGridRow={true}>
+    <span>
       <SEO pathname={location.pathname} />
       <Section narrow>
         <SearchContainer>
@@ -73,54 +75,57 @@ const SearchPage: Template = ({ location, pageContext }) => {
         </SearchContainer>
       </Section>
       <ArticlesGradient />
-    </Layout>
+    </span>
   );
 };
 
 export default SearchPage;
 
-const Horizontal = styled.div`
-  position: relative;
-  margin: 20px auto 50px;
-  border-bottom: 2px solid ${p => p.theme.colors.horizontalNav};
+const Horizontal = styled.div((p: ITAOAThemeUIContext) => ({
+  position: 'relative',
+  margin: '20px auto 50px',
+  borderBottom: `2px solid ${p.theme.colors.horizontalNav}`,
 
-  ${mediaqueries.tablet`
-margin: 20px auto;
-`}
-`;
+  [mediaquery.tablet()]: {
+    margin: '20px auto'
+  }
+}));
 
-const ArticlesGradient = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 590px;
-  z-index: 0;
-  pointer-events: none;
-  background: ${p => p.theme.colors.gradient};
-  transition: ${p => p.theme.colorModeTransition};
-`;
+const ArticlesGradient = styled.div((p: ITAOAThemeUIContext) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  height: '590px',
+  zIndex: 0,
+  pointerEvents: 'none',
+  background: p.theme.colors.gradient as CSS.ColorProperty,
+  transition: p.theme.colorModeTransition
+}));
 
-const ArticlesPaginator = styled.div<{ show: boolean }>`
-  ${p => p.show && `margin-top: 95px;`}
-`;
+interface IArticlesPaginator extends ITAOAThemeUIContext {
+  show: boolean;
+}
+const ArticlesPaginator = styled.div((p: IArticlesPaginator) => ({
+  ...(p.show && { marginTop: '95px' })
+}));
 
-const SearchContainer = styled.div`
-  position: relative;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  padding: 200px 30px 0;
-  background: ${p => p.theme.colors.background};
-  transition: ${p => p.theme.colorModeTransition};
+const SearchContainer = styled.div((p: ITAOAThemeUIContext) => ({
+  position: 'relative',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  padding: '200px 30px 0',
+  background: p.theme.colors.background as CSS.ColorProperty,
+  transition: p.theme.colorModeTransition,
 
-  ${mediaqueries.phablet`
-padding: 150px 30px 0;
-`}
-`;
+  [mediaquery.phablet()]: {
+    padding: '150px 30px 0'
+  }
+}));
 
-const SearchHeading = styled(Headings.h2)`
-  ${mediaqueries.phablet`
-padding: 0;
-`}
-`;
+const SearchHeading = styled(Headings.h2)({
+  [mediaquery.phablet()]: {
+    padding: 0
+  }
+});
