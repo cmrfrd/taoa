c386de46e2d102520adb86cc5a47ebd8cdbae71e
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 23:33:15 -0400

    make should update scroll false

--- src/gatsby/browser/shouldUpdateScroll.js
+++ src/gatsby/browser/shouldUpdateScroll.js
@@ -1,8 +1,5 @@
 /* eslint-disable */
 
 module.exports = ({ routerProps, prevRouterProps, getSavedScrollPosition }) => {
-    const currentPosition = getSavedScrollPosition(routerProps.location,
-                                                   routerProps.location.key);
-    window.scrollTo(...(currentPosition || [0, 0]));
-    return false;
+    return false
 };
