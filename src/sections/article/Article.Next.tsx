1f52ebec75956aa6a658b8c1fab634440cf15a39
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 23:35:41 -0400

    lint

--- src/sections/article/Article.Next.tsx
+++ src/sections/article/Article.Next.tsx
@@ -1,32 +1,30 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { css } from "@emotion/core";
-import { Link } from "gatsby";
+import Headings from '@components/Headings';
+import Image from '@components/Image';
+import mediaqueries from '@styles/media';
+import { IArticle } from '@types';
 
-import Headings from "@components/Headings";
-import Image from "@components/Image";
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+import React from 'react';
 
-import mediaqueries from "@styles/media";
-
-import { IArticle } from "@types";
-
-interface ArticlesNextProps {
-  articles: IArticle[]
+interface IArticlesNextProps {
+  articles: IArticle[];
 }
 
 /**
  * Sits at the bottom of our Article page. Shows the next 2 on desktop and the
  * next 1 on mobile!
  *
- *  [..............], [.........]
- *  [.....LONG.....], [..SHORT..]
- *  [..............], [.........]
+ *  [........], [........]
+ *  [..NORM..], [..NORM..]
+ *  [........], [........]
  *
  * This does NOT use Articles.List because there's a special case of only have 1 article
  * as the next one suggested article, which requires special styling we didn't want to
  * mix into the generic list component.
  */
-const ArticlesNext: React.FC<ArticlesNextProps> = ({ articles }) => {
+const ArticlesNext: React.FC<IArticlesNextProps> = ({ articles }: IArticlesNextProps) => {
   if (!articles) return null;
   const numberOfArticles = articles.length;
   return (
@@ -39,41 +37,36 @@ const ArticlesNext: React.FC<ArticlesNextProps> = ({ articles }) => {
 
 export default ArticlesNext;
 
-interface GridItemProps {
+interface IGridItemProps {
   article: IArticle;
   narrow?: boolean;
 }
 
-const GridItem: React.FC<GridItemProps> = ({ article, narrow }) => {
+const GridItem: React.FC<IGridItemProps> = ({ article, narrow }: IGridItemProps) => {
   if (!article) return null;
 
   const hasOverflow = narrow && article.title.length > 35;
   const imageSource = narrow ? article.hero.narrow : article.hero.regular;
 
   return (
-    <ArticleLink
-      to={article.slug}
-      data-a11y="false"
-      narrow={narrow ? "true" : "false"}
-    >
+    <ArticleLink to={article.slug} data-a11y="false" narrow={narrow ? 'true' : 'false'}>
       <Item>
         <ImageContainer>
           <Image src={imageSource} />
         </ImageContainer>
-        <Title dark hasOverflow={hasOverflow}>
-          {article.title}
-        </Title>
+        <Title hasOverflow={hasOverflow}>{article.title}</Title>
         <Excerpt hasOverflow={hasOverflow}>{article.excerpt}</Excerpt>
         <MetaData>
           {article.date} · {article.timeToRead} min read
-        </MetaData>{" "}
+        </MetaData>{' '}
       </Item>
     </ArticleLink>
   );
 };
 
-const wide = "1fr";
-const narrow = "457px";
+const wide = '1fr';
+/* const narrow = '527px'; */
+const narrow = '0.75fr';
 
 const limitToTwoLines = css`
   text-overflow: ellipsis;
@@ -85,8 +78,8 @@ const limitToTwoLines = css`
   overflow: hidden;
 
   ${mediaqueries.phablet`
-    -webkit-line-clamp: 3;
-  `}
+                       -webkit-line-clamp: 3;
+`}
 `;
 const Grid = styled.div<{ numberOfArticles: number }>`
   position: relative;
@@ -94,55 +87,54 @@ const Grid = styled.div<{ numberOfArticles: number }>`
   ${p => {
     if (p.numberOfArticles === 1) {
       return `
-      grid-template-columns: 1fr;
-      grid-template-rows: 1
-    `;
+grid-template-columns: 1fr;
+grid-template-rows: 1
+`;
     } else {
       return `
-      grid-template-columns: ${wide} ${narrow};
-      grid-template-rows: 2;
-      `;
+grid-template-columns: ${narrow} ${narrow};
+grid-template-rows: 2;
+`;
     }
   }}
   column-gap: 30px;
   margin: 0 auto;
-  max-width: ${p => (p.numberOfArticles === 1 ? "680px" : "100%")};
+  max-width: ${p => (p.numberOfArticles === 1 ? '680px' : '100%')};
 
   ${mediaqueries.desktop`
-    grid-template-columns: 1fr 1fr;
-  `}
+grid-template-columns: 1fr 1fr;
+`}
 
   ${mediaqueries.tablet`
-    grid-template-columns: 1fr;
-  `}
+grid-template-columns: 1fr;
+`}
 `;
 
 const ImageContainer = styled.div`
   position: relative;
-  height: 280px;
+  height: 240px;
   box-shadow: 0 30px 60px -10px rgba(0, 0, 0, ${p => (p.narrow ? 0.22 : 0.3)}),
     0 18px 36px -18px rgba(0, 0, 0, ${p => (p.narrow ? 0.25 : 0.33)});
   margin-bottom: 30px;
-  transition: transform 0.3s var(--ease-out-quad),
-    box-shadow 0.3s var(--ease-out-quad);
+  transition: transform 0.3s var(--ease-out-quad), box-shadow 0.3s var(--ease-out-quad);
 
   & > div {
     height: 100%;
   }
 
   ${mediaqueries.tablet`
-    height: 220px;
-    margin-bottom: 35px;
-  `}
+height: 220px;
+margin-bottom: 35px;
+`}
 
   ${mediaqueries.phablet`
-    height: 200px;
-    margin-bottom: 0;
-    box-shadow: none;
-    overflow: hidden;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-  `}
+height: 200px;
+margin-bottom: 0;
+box-shadow: none;
+overflow: hidden;
+border-top-right-radius: 5px;
+border-top-left-radius: 5px;
+`}
 `;
 
 const Item = styled.div`
@@ -159,20 +151,20 @@ const Item = styled.div`
 const Title = styled(Headings.h3)`
   font-size: 22px;
   line-height: 1.4;
-  margin-bottom: ${p => (p.hasOverflow ? "45px" : "10px")};
+  margin-bottom: ${p => (p.hasOverflow ? '45px' : '10px')};
   color: ${p => p.theme.colors.primary};
   font-family: ${p => p.theme.fonts.serif};
   transition: color 0.3s ease-in-out;
   ${limitToTwoLines};
 
   ${mediaqueries.tablet`
-    margin-bottom: 15px;
-  `}
+margin-bottom: 15px;
+`}
   ${mediaqueries.phablet`
-    padding: 30px 20px 0;
-    margin-bottom: 10px;
-    -webkit-line-clamp: 3;
-  `}
+padding: 30px 20px 0;
+margin-bottom: 10px;
+             -webkit-line-clamp: 3;
+`}
 `;
 
 const Excerpt = styled.p<{ narrow: boolean; hasOverflow: boolean }>`
@@ -180,23 +172,23 @@ const Excerpt = styled.p<{ narrow: boolean; hasOverflow: boolean }>`
   font-size: 16px;
   margin-bottom: 10px;
   color: ${p => p.theme.colors.grey};
-  display: ${p => (p.hasOverflow ? "none" : "box")};
-  max-width: ${p => (p.narrow ? "415px" : "515px")};
+  display: ${p => (p.hasOverflow ? 'none' : 'box')};
+  max-width: ${p => (p.narrow ? '415px' : '515px')};
 
   ${mediaqueries.desktop`
-    display: -webkit-box;
-  `}
+display: -webkit-box;
+`}
 
   ${mediaqueries.tablet`
-    margin-bottom: 15px;
-  `}
+margin-bottom: 15px;
+`}
 
   ${mediaqueries.phablet`
-    max-width: 100%;
-    padding:  0 20px;
-    margin-bottom: 20px;
-    -webkit-line-clamp: 3;
-  `}
+max-width: 100%;
+padding:  0 20px;
+margin-bottom: 20px;
+                        -webkit-line-clamp: 3;
+`}
 `;
 
 const MetaData = styled.div`
@@ -206,9 +198,9 @@ const MetaData = styled.div`
   opacity: 0.33;
 
   ${mediaqueries.phablet`
-    max-width: 100%;
-    padding:  0 20px 30px;
-  `}
+max-width: 100%;
+padding:  0 20px 30px;
+`}
 `;
 
 const ArticleLink = styled(Link)<{ narrow: string }>`
@@ -224,8 +216,7 @@ const ArticleLink = styled(Link)<{ narrow: string }>`
 
   &:hover ${ImageContainer} {
     transform: translateY(-1px);
-    box-shadow: 0 50px 80px -20px rgba(0, 0, 0, 0.27),
-      0 30px 50px -30px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 50px 80px -20px rgba(0, 0, 0, 0.27), 0 30px 50px -30px rgba(0, 0, 0, 0.3);
   }
 
   &:hover h2,
@@ -233,8 +224,8 @@ const ArticleLink = styled(Link)<{ narrow: string }>`
     color: ${p => p.theme.colors.accent};
   }
 
-  &[data-a11y="true"]:focus::after {
-    content: "";
+  &[data-a11y='true']:focus::after {
+    content: ' ';
     position: absolute;
     left: -2%;
     top: -2%;
@@ -244,16 +235,16 @@ const ArticleLink = styled(Link)<{ narrow: string }>`
     background: rgba(255, 255, 255, 0.01);
   }
 
-  ${p => p.narrow === "true" && mediaqueries.tablet`display: none;`}
+  ${p => p.narrow === 'true' && mediaqueries.tablet`display: none;`}
 
   ${mediaqueries.phablet`
-    &:hover ${ImageContainer} {
-      transform: none;
-      box-shadow: initial;
-    }
+                                &:hover ${ImageContainer} {
+                                    transform: none;
+                                    box-shadow: initial;
+                                }
 
-    &:active {
-      transform: scale(0.97) translateY(3px);
-    }
-  `}
+                                &:active {
+                                    transform: scale(0.97) translateY(3px);
+                                }
+`}
 `;
