1b67437b4dea6356c306d9809178edc927ab79ea
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 23:35:30 -0400

    lint and interfaces

--- src/sections/article/Article.Hero.tsx
+++ src/sections/article/Article.Hero.tsx
@@ -2,18 +2,18 @@ import ArticleAuthors from './Article.Authors';
 
 import Headings from '@components/Headings';
 import Image, { ImagePlaceholder } from '@components/Image';
-import mediaqueries from '@styles/media';
-import { IArticle, IAuthor } from '@types';
+import { mediaquery } from '@styles/media';
+import { IArticle, IAuthor, ITAOAThemeUIContext } from '@types';
 
 import styled from '@emotion/styled';
 import React from 'react';
 
-interface ArticleHeroProps {
+interface IArticleHeroProps {
   article: IArticle;
   authors: IAuthor[];
 }
 
-const ArticleHero: React.FC<ArticleHeroProps> = ({ article, authors }) => {
+const ArticleHero: React.FC<IArticleHeroProps> = ({ article, authors }: IArticleHeroProps) => {
   const hasCoAUthors = authors.length > 1;
   const hasHeroImage =
     article.hero &&
@@ -40,12 +40,12 @@ const ArticleHero: React.FC<ArticleHeroProps> = ({ article, authors }) => {
 
 export default ArticleHero;
 
-const Hero = styled.div`
+const Hero = styled.div(
+  (p: ITAOAThemeUIContext) => `
   padding-top: 8rem;
-
-  ${p => mediaqueries.phablet`
+  ${mediaquery.phablet()} {
             &::before {
-                content: "";
+                content: '""';
                 width: 100%;
                 height: 20px;
                 background: ${p.theme.colors.primary};
@@ -56,7 +56,7 @@ const Hero = styled.div`
             }
 
             &::after {
-                content: "";
+                content: '""';
                 width: 100%;
                 height: 10px;
                 background: ${p.theme.colors.background};
@@ -67,119 +67,127 @@ const Hero = styled.div`
                 border-top-right-radius: 25px;
                 transition: ${p.theme.colorModeTransition};
             }
-`}
-`;
-
-const ArticleMeta = styled.div<{ hasCoAUthors: boolean }>`
-  margin-left: ${p => (p.hasCoAUthors ? '10px' : '0')};
-
-  ${mediaqueries.phablet`
-margin-left: 0;
-`}
-`;
-
-const Header = styled.header`
-    position: relative;
-    z-index: 10;
-    margin:100px auto 120px;
-    padding-left: 68px;
-    max-width: 749px;
-
-    ${mediaqueries.desktop`
-padding-left: 53px;
-max-width: calc(507px + 53px);
-margin: 100px auto 70px;
-`}
-
-  ${mediaqueries.tablet`
-padding-left: 0;
-margin: 100px auto 70px;
-max-width: 480px;
-`}
-
-  ${mediaqueries.phablet`
-margin: 170px auto 180px;
-padding: 0 40px;
-`}
-
-  @media screen and (max-height: 700px) {
-    margin: 100px auto;
-  }
-`;
-
-const HeroHeading = styled(Headings.h1)`
-  font-size: 48px;
-  font-family: ${p => p.theme.fonts.serif};
-  margin-bottom: 25px;
-  font-weight: bold;
-  line-height: 1.32;
-
-  ${mediaqueries.tablet`
-margin-bottom: 20px;
-font-size: 36px;
-`}
-
-  ${mediaqueries.phablet`
-font-size: 32px;
-`}
-`;
-
-const HeroSubtitle = styled.div<{ hasCoAUthors: boolean }>`
-  position: relative;
-  display: flex;
-  font-size: 18px;
-  color: ${p => p.theme.colors.grey};
-
-  ${p => mediaqueries.phablet`
-font-size: 14px;
-flex-direction: column;
-
-${
-  p.hasCoAUthors &&
-  `
-        &::before {
-          content: '';
-          position: absolute;
-          left: -20px;
-          right: -20px;
-          top: -10px;
-          bottom: -10px;
-          border: 1px solid ${p.theme.colors.horizontalRule};
-          opacity: 0.5;
-          border-radius: 5px;
-        }
+   }
     `
+);
+
+interface IArticleMeta extends ITAOAThemeUIContext {
+  hasCoAUthors: boolean;
+}
+const ArticleMeta = styled.div((p: IArticleMeta) => ({
+  marginLeft: p.hasCoAUthors ? '10px' : '0',
+
+  [mediaquery.phablet()]: {
+    marginLeft: p.hasCoAUthors ? '10px' : '40px'
+  }
+}));
+
+const Header = styled.header({
+  position: 'relative',
+  zIndex: 10,
+  margin: '100px auto 120px',
+  maxWidth: '549px',
+
+  [mediaquery.desktop()]: {
+    paddingLeft: '40px',
+    paddingRight: '40px',
+    maxWidth: 'calc(549px + 53px)',
+    margin: '100px auto 70px'
+  },
+
+  [mediaquery.tablet()]: {
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    margin: '100px auto 70px',
+    maxWidth: 'calc(540px)'
+  },
+
+  [mediaquery.phablet()]: {
+    margin: '170px auto 180px',
+    padding: '0 20px',
+    maxWidth: '480px'
+  },
+
+  '@media screen and (max-height: 700px)': {
+    margin: '100px auto'
+  }
+});
+
+const HeroHeading = styled(Headings.h1)((p: ITAOAThemeUIContext) => ({
+  fontSize: '48px',
+  fontFamily: p.theme.fonts.serif,
+  marginBottom: '25px',
+  fontWeight: 'bold',
+  lineHeight: 1.32,
+
+  [mediaquery.tablet()]: {
+    marginBottom: '20px',
+    fontSize: '36px'
+  },
+
+  [mediaquery.phablet()]: {
+    fontSize: '32px'
+  }
+}));
+
+interface IHeroSubtitle extends ITAOAThemeUIContext {
+  hasCoAUthors: boolean;
 }
 
+const HeroSubtitle = styled.div(
+  (p: IHeroSubtitle) => `
+     position: relative;
+     display: flex;
+     font-size: 18px;
+     color: ${p.theme.colors.grey};
 
-strong {
-    display: block;
-    font-weight: 500;
-    margin-bottom: 5px;
-}
-`}
-`;
+     ${mediaquery.phablet()} {
+         font-size: 14px;
+         flex-direction: column;
 
-const HeroImage = styled.div`
-  position: relative;
-  z-index: 1;
-  width: 100%;
-  overflow: hidden;
-  margin: 0 auto;
-  box-shadow: 0 30px 60px -10px rgba(0, 0, 0, 0.2), 0 18px 36px -18px rgba(0, 0, 0, 0.22);
-  width: 900px;
+         ${
+           p.hasCoAUthors &&
+           `&::before {
+        content: ' ';
+        position: absolute;
+        left: -5px;
+        right: -10px;
+        top: -10px;
+        bottom: -10px;
+        border: 1px solid ${p.theme.colors.horizontalRule};
+        opacity: 0.5;
+        border-radius: 5px;
+    }`
+         }
 
-  ${mediaqueries.desktop`
-width: calc(80vw - 40px);
-`}
+         strong {
+           display: block;
+           font-weight: 500;
+           margin-bottom: 5px;
+         }
+     }
+    `
+);
 
-  ${mediaqueries.phablet`
-width: calc(100vw - 40px);
-margin: 0 auto;
-height: 220px;
+const HeroImage = styled.div({
+  position: 'relative',
+  zIndex: 1,
+  overflow: 'hidden',
+  margin: '0 auto',
+  boxShadow: '0 30px 60px -10px rgba(0, 0, 0, 0.2), 0 18px 36px -18px rgba(0, 0, 0, 0.22)',
+  width: '900px',
 
-                & > div {
-                    height: 220px;
-                }
-`}
-`;
+  [mediaquery.desktop()]: {
+    width: 'calc(80vw + 20px)'
+  },
+
+  [mediaquery.phablet()]: {
+    width: 'calc(100vw - 40px)',
+    margin: '0 auto',
+    height: '220px',
+
+    '& > div': {
+      height: '220px'
+    }
+  }
+});
