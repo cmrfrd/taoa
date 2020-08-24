336629d2d81898dc6857ba2431b5aa5bb9a7e64e
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-23 23:37:20 -0400

    lint article template

--- src/templates/article.template.tsx
+++ src/templates/article.template.tsx
@@ -1,177 +1,163 @@
-import React, { useRef, useState, useEffect } from "react";
-import styled from "@emotion/styled";
-import throttle from "lodash/throttle";
-import { graphql, useStaticQuery } from "gatsby";
+import ArticleAside from '../sections/article/Article.Aside';
+import ArticleHero from '../sections/article/Article.Hero';
+import ArticlesNext from '../sections/article/Article.Next';
+import ArticleSEO from '../sections/article/Article.SEO';
+import ArticleShare from '../sections/article/Article.Share';
 
-import Layout from "@components/Layout";
-import MDXRenderer from "@components/MDX";
-import Progress from "@components/Progress";
-import Section from "@components/Section";
-import Subscription from "@components/Subscription";
+import MDX from '@components/MDX';
+import Section from '@components/Section';
+import { mediaquery } from '@styles/media';
+import { Template, TTemplate, ITAOAThemeUIContext } from '@types';
+import { debounce } from '@utils';
 
-import mediaqueries from "@styles/media";
-import { debounce } from "@utils";
-
-import ArticleAside from "../sections/article/Article.Aside";
-import ArticleHero from "../sections/article/Article.Hero";
-import ArticleControls from "../sections/article/Article.Controls";
-import ArticlesNext from "../sections/article/Article.Next";
-import ArticleSEO from "../sections/article/Article.SEO";
-import ArticleShare from "../sections/article/Article.Share";
-
-import { Template } from "@types";
+import styled from '@emotion/styled';
+import { graphql, useStaticQuery } from 'gatsby';
+import throttle from 'lodash/throttle';
+import React, { useRef, useState, useEffect } from 'react';
 
 const siteQuery = graphql`
-    {
-        allSite {
-            edges {
-                node {
-                    siteMetadata {
-                        name
-                    }
-                }
-            }
+  {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            name
+          }
         }
+      }
     }
+  }
 `;
 
-const Article: Template = ({ pageContext, location }) => {
-    const contentSectionRef = useRef<HTMLElement>(null);
+const Article: Template = ({ pageContext, location }: TTemplate) => {
+  const contentSectionRef = useRef<HTMLElement>(null);
 
-    const [hasCalculated, setHasCalculated] = useState<boolean>(false);
-    const [contentHeight, setContentHeight] = useState<number>(0);
+  const [hasCalculated, setHasCalculated] = useState<boolean>(false);
+  const [contentHeight, setContentHeight] = useState<number>(0);
 
-    const results = useStaticQuery(siteQuery);
-    const name = results.allSite.edges[0].node.siteMetadata.name;
+  const results = useStaticQuery(siteQuery);
+  const name = results.allSite.edges[0].node.siteMetadata.name;
 
-    const { article, authors, next } = pageContext;
+  const { article, authors, next } = pageContext;
 
-    useEffect(() => {
-        const calculateBodySize = throttle(() => {
-            const contentSection = contentSectionRef.current;
+  useEffect(() => {
+    const calculateBodySize = throttle(() => {
+      const contentSection = contentSectionRef.current;
 
-            if (!contentSection) return;
+      if (!contentSection) return;
 
-            /**
-             * If we haven't checked the content's height before,
-             * we want to add listeners to the content area's
-             * imagery to recheck when it's loaded
-             */
-            if (!hasCalculated) {
-                const debouncedCalculation = debounce(calculateBodySize);
-                const $imgs = contentSection.querySelectorAll("img");
+      /**
+       * If we haven't checked the content's height before,
+       * we want to add listeners to the content area's
+       * imagery to recheck when it's loaded
+       */
+      if (!hasCalculated) {
+        const debouncedCalculation = debounce(calculateBodySize);
+        const $imgs = contentSection.querySelectorAll('img');
 
-                $imgs.forEach($img => {
-                    // If the image hasn't finished loading then add a listener
-                    if (!$img.complete) $img.onload = debouncedCalculation;
-                });
+        $imgs.forEach(($img: HTMLImageElement) => {
+          // If the image hasn't finished loading then add a listener
+          if (!$img.complete) $img.onload = debouncedCalculation;
+        });
 
-                // Prevent rerun of the listener attachment
-                setHasCalculated(true);
-            }
+        // Prevent rerun of the listener attachment
+        setHasCalculated(true);
+      }
 
-            // Set the height and offset of the content area
-            setContentHeight(contentSection.getBoundingClientRect().height);
-        }, 20);
+      // Set the height and offset of the content area
+      setContentHeight(contentSection.getBoundingClientRect().height);
+    }, 20);
 
-        calculateBodySize();
-        window.addEventListener("resize", calculateBodySize);
+    calculateBodySize();
+    window.addEventListener('resize', calculateBodySize);
 
-        return () => window.removeEventListener("resize", calculateBodySize);
-    }, []);
+    return (): void => window.removeEventListener('resize', calculateBodySize);
+  }, []);
 
-    return (
-        <Layout location={location} enableGridRow={false}>
-            <ArticleSEO article={article} authors={authors} location={location} />
-            <ArticleHero article={article} authors={authors} />
-            <ArticleAside contentHeight={contentHeight}>
-                <Progress contentHeight={contentHeight} />
-            </ArticleAside>
-            <ArticleBody ref={contentSectionRef}>
-                <MDXRenderer content={article.body}>
-                    <ArticleShare />
-                </MDXRenderer>
-            </ArticleBody>
-            {next.length > 0 && (
-                <NextArticle narrow>
-                    <FooterNext>More articles from {name}</FooterNext>
-                    <ArticlesNext articles={next} />
-                    <FooterSpacer />
-                </NextArticle>
-            )}
-        </Layout>
-    );
+  return (
+    <span>
+      <ArticleSEO article={article} authors={authors} location={location} />
+      <ArticleHero article={article} authors={authors} />
+      <ArticleAside contentHeight={contentHeight}></ArticleAside>
+      <ArticleBody ref={contentSectionRef}>
+        <MDX content={article.body}>
+          <ArticleShare />
+        </MDX>
+      </ArticleBody>
+      {next.length > 0 && (
+        <NextArticle narrow>
+          <FooterNext>More articles from {name}</FooterNext>
+          <ArticlesNext articles={next} />
+          <FooterSpacer />
+        </NextArticle>
+      )}
+    </span>
+  );
 };
 
 export default Article;
 
-const MobileControls = styled.div`
-    position: relative;
-    padding-top: 60px;
-    transition: background 0.2s linear;
-    text-align: center;
+const ArticleBody = styled.article({
+  position: 'relative',
+  padding: '160px 0 35px',
+  transition: 'background 0.2s linear',
 
-    ${mediaqueries.tablet_up`
-display: none;
-`}
-`;
+  [mediaquery.desktop()]: {
+    padding: '160px 0 35px'
+  },
 
-const ArticleBody = styled.article`
-    position: relative;
-    padding: 160px 0 35px;
-    transition: background 0.2s linear;
+  [mediaquery.tablet()]: {
+    padding: '160px 10px 80px'
+  },
 
-    ${mediaqueries.desktop`
-padding: 160px 0 35px;
-`}
-
-  ${mediaqueries.tablet`
-padding: 160px 0 80px;
-`}
-
-  ${mediaqueries.phablet`
-padding: 160px 0;
-`}
-`;
-
-const NextArticle = styled(Section)`
-  display: block;
-`;
-
-const FooterNext = styled.h3`
-    position: relative;
-    opacity: 0.25;
-    margin-bottom: 100px;
-    font-weight: 400;
-    color: ${p => p.theme.colors.primary};
-
-    ${mediaqueries.tablet`
-margin-bottom: 60px;
-`}
-
-  &::after {
-    content: '';
-    position: absolute;
-    background: ${p => p.theme.colors.grey};
-    width: ${(910 / 1140) * 100}%;
-    height: 1px;
-    right: 0;
-    top: 11px;
-
-    ${mediaqueries.tablet`
-width: ${(600 / 1140) * 100}%;
-`}
-
-    ${mediaqueries.phablet`
-width: ${(400 / 1140) * 100}%;
-`}
-
-    ${mediaqueries.phone`
-width: 90px
-`}
+  [mediaquery.phablet()]: {
+    padding: '160px 0'
   }
-`;
+});
 
-const FooterSpacer = styled.div`
-    margin-bottom: 65px;
-`;
+const NextArticle = styled(Section)({
+  display: 'block'
+});
+
+const FooterNext = styled.h3((p: ITAOAThemeUIContext) => ({
+  position: 'relative',
+  opacity: 0.25,
+  marginBottom: '100px',
+  fontWeight: 400,
+  color: `${p.theme.colors.primary}`,
+
+  [mediaquery.tablet()]: {
+    marginBottom: '60px'
+  },
+
+  [mediaquery.phone()]: {
+    textAlign: 'center'
+  },
+
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    background: `${p.theme.colors.grey}`,
+    width: `calc(100% - 290px)`,
+    height: '1.25px',
+    right: 0,
+    top: '11px',
+
+    [mediaquery.tablet()]: {
+      height: '1px',
+      width: `${(600 / 1140) * 80}%`
+    },
+
+    [mediaquery.phablet()]: {
+      width: `calc(100% - 290px)`
+    },
+
+    [mediaquery.phone()]: {
+      width: `${0}%`
+    }
+  }
+}));
+
+const FooterSpacer = styled.div({
+  marginBottom: '65px'
+});
