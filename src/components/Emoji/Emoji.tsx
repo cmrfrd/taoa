c0abb4ad1d72eb6c12812e3001e8da62c7f56cea
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-28 23:29:28 -0400

    Add emoji component

---
+++ src/components/Emoji/Emoji.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import EmojioneV4, { Props } from 'react-emoji-render';
+import { graphql, useStaticQuery } from 'gatsby';
+
+const nameQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        emojiDir
+      }
+    }
+  }
+`;
+
+const emojiDir = (): string => useStaticQuery(nameQuery).site.siteMetadata.emojiDir;
+
+const Emoji: React.FC<Props> = (props: Props): JSX.Element => {
+  const { text, options } = props;
+
+  return (
+    <EmojioneV4
+      options={{
+        protocol: 'https',
+        baseUrl: `/${emojiDir()}/png/`,
+        ext: 'png',
+        size: '128',
+        ...options
+      }}
+      text={text}
+      onlyEmojiClassName="make-emojis-large"
+    />
+  );
+};
+
+export default Emoji;
