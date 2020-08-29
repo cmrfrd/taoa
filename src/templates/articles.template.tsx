27d3b2f9846e3df3e7cfd931b4bd9bf2206e2e78
cmrfrd <alexanderjcomerford@gmail.com>
2020-08-29 18:57:01 -0400

    update articles page
    lint
    rework styled compoentns

--- src/templates/articles.template.tsx
+++ src/templates/articles.template.tsx
@@ -1,49 +1,52 @@
-import React from "react";
-import styled from "@emotion/styled";
+import ArticlesHero from '../sections/articles/Articles.Hero';
+import ArticlesList from '../sections/articles/Articles.List';
 
-import Section from "@components/Section";
-import SEO from "@components/SEO";
-import Layout from "@components/Layout";
-import Paginator from "@components/Navigation/Navigation.Paginator";
+import Layout from '@components/Layout';
+import Paginator from '@components/Navigation/Navigation.Paginator';
+import SEO from '@components/SEO';
+import Section from '@components/Section';
+import { Template, TTemplate, ITAOAThemeUIContext } from '@types';
 
-import ArticlesHero from "../sections/articles/Articles.Hero";
-import ArticlesList from "../sections/articles/Articles.List";
+import styled from '@emotion/styled';
+import * as CSS from 'csstype';
+import React from 'react';
 
-import { Template } from "@types";
+const ArticlesPage: Template = ({ location, pageContext }: TTemplate) => {
+  const { articles } = pageContext;
 
-const ArticlesPage: Template = ({ location, pageContext }) => {
-    const articles = pageContext.group;
-    const authors = pageContext.additionalContext.authors;
-
-    return (
-        <Layout location={location} enableGridRow={true}>
-            <SEO pathname={location.pathname} />
-            <ArticlesHero authors={authors} />
-            <Section narrow>
-                <ArticlesList articles={articles} />
-                <ArticlesPaginator show={pageContext.pageCount > 1}>
-                    <Paginator {...pageContext} />
-                </ArticlesPaginator>
-            </Section>
-            <ArticlesGradient />
-        </Layout>
-    );
+  return (
+    <span>
+      <SEO pathname={location.pathname} />
+      <ArticlesHero />
+      <Section narrow>
+        <ArticlesList articles={articles} />
+        <ArticlesPaginator show={pageContext.pageCount > 1}>
+          <Paginator {...pageContext} />
+        </ArticlesPaginator>
+      </Section>
+      <ArticlesGradient />
+    </span>
+  );
 };
 
 export default ArticlesPage;
 
-const ArticlesGradient = styled.div`
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 590px;
-    z-index: 0;
-    pointer-events: none;
-    background: ${p => p.theme.colors.gradient};
-    transition: ${p => p.theme.colorModeTransition};
-`;
+const ArticlesGradient = styled.div((p: ITAOAThemeUIContext) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  width: '100%',
+  height: '590px',
+  zIndex: 0,
+  pointerEvents: 'none',
+  background: p.theme.colors.gradient as CSS.ColorProperty,
+  transition: p.theme.colorModeTransition
+}));
 
-const ArticlesPaginator = styled.div<{ show: boolean }>`
-  ${p => p.show && `margin-top: 95px;`}
-  `;
+interface IArticlesPaginator extends ITAOAThemeUIContext {
+  show: boolean;
+}
+
+const ArticlesPaginator = styled.div<{ show: boolean }>((p: IArticlesPaginator) => ({
+  ...(p.show && { marginTop: '95px' })
+}));
