797f7e1d4f5ac7dccc8c8188fc0f9ee2a2f8c05b
cmrfrd <alexanderjcomerford@gmail.com>
2020-09-02 18:19:17 -0400

    Update search page
    - add/remove/reorder imports
    - get pageLength from graphql
    - calculate current page and pages
    - render paginator and remove gradient
    - remove background from container preventing cover glitch
      during transition

--- src/templates/search.template.tsx
+++ src/templates/search.template.tsx
@@ -1,19 +1,21 @@
 import ArticlesList from '../sections/articles/Articles.List';
 
 import Headings from '@components/Headings';
-import Layout from '@components/Layout';
 import Paginator from '@components/Navigation/Navigation.Paginator';
 import SEO from '@components/SEO';
 import Search from '@components/Search';
 import Section from '@components/Section';
-import mediaqueries, { mediaquery } from '@styles/media';
+import mediaqueries, { mediaquery, mediaqueryup } from '@styles/media';
 import { Template, TTemplate, IArticle, ITAOAThemeUIContext } from '@types';
+import { motion, AnimatePresence } from 'framer-motion';
 
+import { css } from '@emotion/core';
+import { SerializedStyles } from '@emotion/serialize';
 import styled from '@emotion/styled';
-import { graphql, useStaticQuery } from 'gatsby';
-import React, { useState } from 'react';
-
 import * as CSS from 'csstype';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
+import React, { useState } from 'react';
 
 const siteQuery = graphql`
   {
@@ -24,6 +26,7 @@ const siteQuery = graphql`
             search {
               placeholder
               heading
+              pageLength
             }
           }
         }
@@ -39,10 +42,14 @@ const SearchPage: Template = ({ location, pageContext }: TTemplate) => {
   const { search } = results.allSite.edges[0].node.siteMetadata;
 
   const [searchResults, setSearchResults] = useState(articles);
+  const pages = Math.ceil([...searchResults].length / search.pageLength);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   const filter = (e: IArticle, term: string): boolean => {
     return e.title.toLowerCase().includes(term) || e.title.includes(term);
   };
+
   const sort = (a: IArticle, b: IArticle): number => {
     if (a.dateForSEO > b.dateForSEO) {
       return -1;
@@ -68,13 +75,32 @@ const SearchPage: Template = ({ location, pageContext }: TTemplate) => {
             placeholder={search.placeholder}
           />
           <Horizontal />
-          <ArticlesList articles={searchResults} />
+          <ArticlesList
+            articles={searchResults.slice(
+              (currentPage - 1) * search.pageLength,
+              currentPage * search.pageLength
+            )}
+            currentPage={currentPage}
+          />
           <ArticlesPaginator show={pageContext.pageCount > 1}>
-            <Paginator {...pageContext} />
+            <Paginator
+              {...{
+                index: 1,
+                pageCount: pages,
+                count: searchResults.slice(
+                  (currentPage - 1) * search.pageLength,
+                  currentPage * search.pageLength
+                ).length,
+                pathPrefix: '/bluh',
+                setCurrentPage: setCurrentPage,
+                currentPage: currentPage,
+                ...pageContext
+              }}
+            />
           </ArticlesPaginator>
+          <div>{pages}</div>
         </SearchContainer>
       </Section>
-      <ArticlesGradient />
     </span>
   );
 };
@@ -114,9 +140,9 @@ const SearchContainer = styled.div((p: ITAOAThemeUIContext) => ({
   position: 'relative',
   bottom: 0,
   left: 0,
+  zIndex: 1,
   width: '100%',
   padding: '200px 30px 0',
-  background: p.theme.colors.background as CSS.ColorProperty,
   transition: p.theme.colorModeTransition,
 
   [mediaquery.phablet()]: {
