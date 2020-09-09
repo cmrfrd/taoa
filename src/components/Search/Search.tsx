da653f874b9906aab2cbe163b2608565a1a8d1e1
cmrfrd <alexanderjcomerford@gmail.com>
2020-09-09 11:55:01 -0400

    Improve the search bar
    - Add more state hooks
    - Add spinner
    - Fix the timings
    - small cosmetics

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,12 +4,15 @@ import { theme } from '@utils';
 
 import styled from '@emotion/styled';
 import * as CSS from 'csstype';
-import _ from 'lodash';
-import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import React, { useEffect, useState, useRef } from 'react';
 import { useColorMode } from 'theme-ui';
 
 const Search: React.FC<{}> = (props: any) => {
-  const { elements, searchResults, setSearchResults, placeholder, filter, sort } = props;
+  const { elements, setSearchResults, placeholder, filter, sort } = props;
+  const { setCurrentPage } = props;
+  const { searching, setSearching } = props;
+  const { setNumSearchResults } = props;
 
   const [colorMode] = useColorMode();
   const tcolors = colorMode === 'dark' ? theme.colors.modes.dark : theme.colors;
@@ -19,20 +22,49 @@ const Search: React.FC<{}> = (props: any) => {
     setSearchTerm(event.target.value);
   };
 
+  const resultsTimeoutRef = useRef(null);
   useEffect(() => {
+    if (resultsTimeoutRef.current !== null) {
+      clearTimeout(resultsTimeoutRef.current);
+    }
+
     const results = elements.filter((e: any) => filter(e, searchTerm)).sort(sort);
+    console.log('num results', results.length);
 
-    if (_.isEqual(results.sort(), searchResults.sort())) return;
-
-    setTimeout(() => {
+    resultsTimeoutRef.current = setTimeout(() => {
+      resultsTimeoutRef.current = null;
       setSearchResults(results);
-    }, 200);
+      setNumSearchResults(results.length);
+    }, 480);
+
     setSearchResults([]);
+  }, [searchTerm]);
+
+  const searchingTimeoutRef = useRef(null);
+  useEffect(() => {
+    if (searchingTimeoutRef.current !== null) {
+      clearTimeout(searchingTimeoutRef.current);
+    }
+
+    searchingTimeoutRef.current = setTimeout(() => {
+      searchingTimeoutRef.current = null;
+      console.log('not searching');
+      setSearching(false);
+    }, 500);
+
+    setSearching(true);
+    setCurrentPage(0);
+    console.log('searching');
   }, [searchTerm]);
 
   return (
     <InputContainer>
       <IconContainer>
+        {searching && (
+          <LoaderContainer>
+            <CircleLoader />
+          </LoaderContainer>
+        )}
         <Icons.SearchIcon fill={tcolors.invbackground} />
       </IconContainer>
       <Input type="text" placeholder={placeholder} onChange={handleChange} />
@@ -41,6 +73,40 @@ const Search: React.FC<{}> = (props: any) => {
 };
 
 export default Search;
+
+const CircleLoader: React.FC<{}> = () => {
+  const spinTransition = {
+    loop: Infinity,
+    ease: 'linear',
+    duration: 1
+  };
+
+  return (
+    <Container>
+      <CircleLoaderContainer animate={{ rotate: 360 }} transition={spinTransition} />
+    </Container>
+  );
+};
+
+const CircleLoaderContainer = styled(motion.span)((p: ITAOAThemeUIContext) => ({
+  display: 'block',
+  width: '2rem',
+  height: '2rem',
+  border: `0.25rem solid ${p.theme.colors.background}`,
+  borderTop: `0.25rem solid ${p.theme.colors.primary}`,
+  borderRadius: '50%',
+  position: 'absolute',
+  boxSizing: 'border-box',
+  top: '0.4rem',
+  left: '0.5rem'
+}));
+
+const Container = styled.div(() => ({
+  position: 'relative',
+  width: '3rem',
+  height: '3rem',
+  boxSizing: 'border-box'
+}));
 
 const InputContainer = styled.div(() => ({
   position: 'relative',
@@ -56,6 +122,11 @@ const InputContainer = styled.div(() => ({
   }
 }));
 
+const LoaderContainer = styled.div(() => ({
+  position: 'absolute',
+  right: '4.5rem'
+}));
+
 const Input = styled.input((p: ITAOAThemeUIContext) => ({
   outline: 0,
   borderWidth: '0 0 2px',
@@ -67,6 +138,4 @@ const Input = styled.input((p: ITAOAThemeUIContext) => ({
   width: '100%'
 }));
 
-const IconContainer = styled.div(() => ({
-  paddingBottom: '4px'
-}));
+const IconContainer = styled.div(() => ({}));
