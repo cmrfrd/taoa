0b24d1805d19fb1ec185216199bdb2dbbec95ff9
cmrfrd <alexanderjcomerford@gmail.com>
2020-10-16 00:35:17 -0400

    add csp policy to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,20 @@ module.exports = {
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-offline`,
     {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          'script-src': "'self' www.google-analytics.com",
+          'img-src': "'self' data: www.google-analytics.com"
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'The Art of Abstraction',
