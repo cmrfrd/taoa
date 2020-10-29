7ab59735e542bac0f38e1e8dc8a080d04e6f1662
cmrfrd <alexanderjcomerford@gmail.com>
2020-10-28 21:39:36 -0400

    Fix and log emoji download script

--- bin/container_download_emojis.sh
+++ bin/container_download_emojis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
 if ! command -v node &> /dev/null
@@ -12,6 +12,7 @@ EMOJI_DIR=$(echo 'require("./gatsby-config").siteMetadata.emojiDir;' |
               node -i |
               awk '{a[NR]=$2} END{print a[NR-1] }' |
               tr -d \')
+[ -z "$EMOJI_DIR" ] && echo "Emoji directory not found ..." && exit 0;
 
 # Configure static/emoji/fileext dir
 STATIC_DIR=static
@@ -19,7 +20,9 @@ EMOJI_DST_DIR=$STATIC_DIR/$EMOJI_DIR
 EMOJI_SRC_DIR=png
 
 ## Make static directory and emoji dir
+echo "Creating directory $STATIC_DIR ..."
 mkdir -p $STATIC_DIR
+echo "Creating directory $EMOJI_DST_DIR ..."
 mkdir -p $EMOJI_DST_DIR
 
 if [[ ! -e $dir ]]; then
@@ -31,7 +34,7 @@ if [[ ! -e $dir ]]; then
   ## Copy into static emoji dir
   cp -r $tmp/$EMOJI_SRC_DIR $EMOJI_DST_DIR
 
-  rm -r $tmp
+  rm -rf $tmp
 
 elif [[ ! -d $EMOJI_DST_DIR ]]; then
   echo "$EMOJI_DST_DIR already exists but is not a directory"
