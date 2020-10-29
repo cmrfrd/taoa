de1fffc712baca30001246df5c7ac5d2f260b250
cmrfrd <alexanderjcomerford@gmail.com>
2020-10-28 21:39:09 -0400

    Update local shell script to use user ns for files

--- bin/local_shell.sh
+++ bin/local_shell.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 source development.env
 ./bin/local_build_image.sh
 docker run \
-       -u $(id -u ${USER}):$(id -g ${USER}) \
-       -w /work \
-       -v $(pwd):/work/:Z \
-       -v ~/.ssh:/.ssh:Z \
-       -v ~/.gitconfig:/etc/gitconfig:Z \
+       -u $(id -u):$(id -g) \
+       --userns=keep-id \
+       -w /home/node/work/ \
+       -v $(pwd):/home/node/work/ \
+       -v ~/.ssh:/home/node/.ssh \
+       -v ~/.gitconfig:/etc/gitconfig \
        --net host \
        --rm \
        -it $IMAGE_NAME:$TAG \
