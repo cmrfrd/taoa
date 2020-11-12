2a80c275065382ed48a43f23aeea093ac0005e98
cmrfrd <alexanderjcomerford@gmail.com>
2020-11-11 22:59:46 -0500

    Stylistic change in about page

--- src/templates/about.template.tsx
+++ src/templates/about.template.tsx
@@ -45,7 +45,7 @@ const AuthorContainer = styled.div((p: ITAOAThemeUIContext) => ({
   left: 0,
   zIndex: 1,
   width: '100%',
-  padding: '150px 30px 0',
+  padding: '150px 10px 0',
   transition: p.theme.colorModeTransition
 }));
 
