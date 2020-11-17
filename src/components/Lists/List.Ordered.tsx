dc1ff782cc7abbdb2f9c35d850274e96465c4e3f
cmrfrd <alexanderjcomerford@gmail.com>
2020-11-16 22:15:36 -0500

    Update ordered and unordered list css

--- src/components/Lists/List.Ordered.tsx
+++ src/components/Lists/List.Ordered.tsx
@@ -6,84 +6,66 @@ import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import * as CSS from 'csstype';
 
-const OrderedList = styled.ol((p: ITAOAThemeUIContext) =>
-  css({
-    listStyle: 'none',
-    counterReset: 'list',
-    color: p.theme.colors.postText as CSS.ColorProperty,
-    position: 'relative',
-    padding: '15px 0 30px 0px',
-    margin: '0 auto',
-    transition: p.theme.colorModeTransition,
-    fontSize: '18px',
+const OrderedList = styled.ol((p: ITAOAThemeUIContext) => ({
+  listStyleType: 'decimal',
+  counterReset: 'ordered-list',
+  color: p.theme.colors.postText as CSS.ColorProperty,
+  padding: '15px 0 30px 0px',
+  margin: '0 auto',
+  transition: p.theme.colorModeTransition,
+  position: 'relative',
+  fontWeight: 400,
 
-    width: '100%',
-    maxWidth: '780px',
+  width: '100%',
+  maxWidth: '780px',
 
-    [mediaquery.desktop()]: {
-      maxWidth: '607px'
-    },
+  [mediaquery.desktop()]: {
+    maxWidth: '607px'
+  },
+
+  [mediaquery.tablet()]: {
+    maxWidth: '586px',
+    paddingLeft: '20px'
+  },
+
+  [mediaquery.phablet()]: {
+    padding: '0 20px',
+    paddingLeft: '20px'
+  },
+
+  [mediaquery.phone()]: {
+    paddingLeft: '40px'
+  },
+
+  li: {
+    paddingBottom: '15px',
+    fontSize: '19px',
 
     [mediaquery.tablet()]: {
-      maxWidth: '586px',
-      paddingLeft: '0px',
-      fontSize: '16px'
+      fontSize: '17px'
     },
 
     [mediaquery.phablet()]: {
-      padding: '0 20px',
-      fontSize: '14px'
+      fontSize: '15px'
     },
 
-    li: {
-      position: 'relative',
-      paddingBottom: '15px',
-
-      [mediaqueries.tablet()]: {
-        paddingLeft: '30px'
-      },
-
-      [mediaqueries.phablet()]: {
-        paddingLeft: '30px'
-      },
-
-      p: {
-        [mediaqueries.tablet()]: {
-          padding: 0
-        }
-      }
-    },
-
-    'li > :not(ol, ul)': {
-      display: 'inline-block',
-      color: 'red'
-    },
-
-    'li > p': {
-      display: 'inline-block',
-      paddingLeft: '3rem'
-    },
-
-    'li::before': {
-      display: 'block',
-      position: 'absolute',
-      color: p.theme.colors.postText as CSS.ColorProperty,
-      content: `counter(list) '.'`,
-      top: '0.2rem',
-      width: '2.5rem',
-      counterIncrement: 'list',
-      fontWeight: 600,
+    p: {
+      paddingLeft: '0px',
+      fontSize: '18px',
 
       [mediaquery.tablet()]: {
-        left: 0,
-        fontSize: '18px'
+        fontSize: '16px'
       },
 
       [mediaquery.phablet()]: {
-        fontSize: '16px'
+        fontSize: '14px'
       }
     }
-  })
-);
+  },
+
+  'li > :not(ol, ul)': {
+    display: 'inline-block'
+  }
+}));
 
 export default OrderedList;
