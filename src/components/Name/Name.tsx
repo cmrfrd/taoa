31193eda6127736f0a669ef4c492646b593b34b8
cmrfrd <alexanderjcomerford@gmail.com>
2021-07-11 23:28:06 -0400

    Update typing for Name

--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -26,27 +26,27 @@ const base = (p: ITAOAThemeUIContext): IStringMap => ({
   padding: '0 0px !important'
 });
 
-const h1: React.FC = (): ReactElement => {
+const h1: React.FC = (): React.ReactElement => {
   const Nameh1 = styled(Headings.h1)(base);
   return <Nameh1>{name()}</Nameh1>;
 };
-const h2: React.FC = (): ReactElement => {
+const h2: React.FC = (): React.ReactElement => {
   const Nameh2 = styled(Headings.h2)(base);
   return <Nameh2>{name()}</Nameh2>;
 };
-const h3: React.FC = (): ReactElement => {
+const h3: React.FC = (): React.ReactElement => {
   const Nameh3 = styled(Headings.h3)(base);
   return <Nameh3>{name()}</Nameh3>;
 };
-const h4: React.FC = (): ReactElement => {
+const h4: React.FC = (): React.ReactElement => {
   const Nameh4 = styled(Headings.h4)(base);
   return <Nameh4>{name()}</Nameh4>;
 };
-const h5: React.FC = (): ReactElement => {
+const h5: React.FC = (): React.ReactElement => {
   const Nameh5 = styled(Headings.h5)(base);
   return <Nameh5>{name()}</Nameh5>;
 };
-const h6: React.FC = (): ReactElement => {
+const h6: React.FC = (): React.ReactElement => {
   const Nameh6 = styled(Headings.h6)(base);
   return <Nameh6>{name()}</Nameh6>;
 };
