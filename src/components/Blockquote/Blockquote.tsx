cda7580c470f88a46aff54ac421cd57887ea2fb2
cmrfrd <alexanderjcomerford@gmail.com>
2021-11-06 04:43:58 -0400

    Update blockquote to be correctly formatted

--- src/components/Blockquote/Blockquote.tsx
+++ src/components/Blockquote/Blockquote.tsx
@@ -1,5 +1,6 @@
 import { mediaquery } from '@styles/media';
 import { ITAOAThemeUIContext } from '@types';
+import Paragraph from '@components/Paragraph';
 
 import styled from '@emotion/styled';
 import * as CSS from 'csstype';
@@ -7,38 +8,21 @@ import * as CSS from 'csstype';
 /**
  * Blockquote is a component for display quotes!
  */
-const Blockquote = styled.blockquote((p: ITAOAThemeUIContext) => ({
+const BBlockquote = styled.blockquote((p: ITAOAThemeUIContext) => ({
   transition: p.theme.colorModeTransition,
-  margin: '15px auto 50px',
   color: p.theme.colors.postText as CSS.ColorProperty,
-  fontFamily: p.theme.fonts.serif,
-  fontStyle: 'italic',
-  textAlign: 'center',
-
-  [mediaquery.tablet()]: {
-    margin: '10px auto 35px'
-  },
-
+  borderLeft: `10px solid ${p.theme.colors.postText}`,
   '& > p': {
-    fontFamily: p.theme.fonts.serif,
-    maxWidth: '880px !important',
-    paddingBottom: 0,
-    width: '100%',
-    margin: '0 auto',
-    fontSize: '36px',
-    lineHeight: 1.32,
-    fontWeight: 'bold',
-
-    [mediaquery.tablet()]: {
-      fontSize: '26px',
-      padding: '0 180px'
-    },
-
-    [mediaquery.phablet()]: {
-      fontSize: '36px',
-      padding: '0 20px'
-    }
+    paddingLeft: '10px'
   }
 }));
+
+const Blockquote: React.FC = ({ children }) => {
+  return (
+    <Paragraph>
+      <BBlockquote>{children}</BBlockquote>
+    </Paragraph>
+  );
+};
 
 export default Blockquote;
