f0cbb6749394898698b40f18705c9de5c4037674
cmrfrd <alexanderjcomerford@gmail.com>
2021-12-27 23:55:15 -0500

    make horizontal lines look better

--- src/components/HorizontalRule/HorizontalRule.tsx
+++ src/components/HorizontalRule/HorizontalRule.tsx
@@ -4,6 +4,10 @@ import { ITAOAThemeUIContext } from '@types';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+
+const H_DARK_SVG = "data:image/svg+xml,<svg width='10' height='15' viewBox='0 0 10 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.432617' y='13.8564' width='16' height='1' transform='rotate(-60 0.432617 13.8564)' fill='%2350525B'/></svg>";
+const H_LIGHT_SVG = "data:image/svg+xml,<svg width='10' height='15' viewBox='0 0 10 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.567383' y='14.1777' width='16' height='1' transform='rotate(-60 0.567383 14.1777)' fill='%232D2E33'/></svg>";
+
 /* This component is a simple horizontal line
  * to divide one section from another
  */
@@ -15,13 +19,7 @@ const HorizontalRule = styled.hr((p: ITAOAThemeUIContext) =>
     margin: '50px auto',
     border: 0,
     height: '14.36px',
-    backgroundImage: `url("
-        ${
-          p.isDark
-            ? "data:image/svg+xml,<svg width='10' height='15' viewBox='0 0 10 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.432617' y='13.8564' width='16' height='1' transform='rotate(-60 0.432617 13.8564)' fill='%2350525B'/></svg>"
-            : "data:image/svg+xml,<svg width='10' height='15' viewBox='0 0 10 15' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0.567383' y='14.1777' width='16' height='1' transform='rotate(-60 0.567383 14.1777)' fill='%232D2E33'/></svg>"
-        }
-    ")`,
+    backgroundImage: `url("${ p.colorMode ? H_DARK_SVG : H_LIGHT_SVG }")`,
 
     backgroundRepeat: 'repeat-x',
     boxSizing: 'border-box',
