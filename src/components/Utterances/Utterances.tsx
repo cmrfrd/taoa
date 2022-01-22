e4fa9fa2f0d2cfc5d8866f3686362e9c7614fde9
cmrfrd <alexanderjcomerford@gmail.com>
2022-01-21 23:23:14 -0500

    Update utterances

--- src/components/Utterances/Utterances.tsx
+++ src/components/Utterances/Utterances.tsx
@@ -1,28 +1,66 @@
 import Section from '@components/Section';
 import styled from '@emotion/styled';
 import { useColorMode } from 'theme-ui';
+import React, { useRef, useEffect } from 'react';
+
+const SOURCE = 'https://utteranc.es/client.js';
+
+/**
+ * Return HTMLScriptElement for async loading utterance script
+ * @param param - `Utterances` setting properties
+ * @returns HTMLScriptElement what load utterances script
+ */
+const createScriptElement = ({ repo, label, theme, issueTerm, issueNumber }): HTMLScriptElement => {
+  const scriptEl = document.createElement('script');
+
+  scriptEl.src = SOURCE;
+  scriptEl.async = true;
+  scriptEl.setAttribute('repo', repo);
+  if (issueTerm) {
+    const _issueTerm = Array.isArray(issueTerm) ? issueTerm.join(' ') : issueTerm;
+
+    scriptEl.setAttribute('issue-term', _issueTerm);
+  } else if (typeof issueNumber === 'number') {
+    scriptEl.setAttribute('issue-number', String(issueNumber));
+  }
+  scriptEl.setAttribute('crossorigin', 'anonymous');
+  scriptEl.setAttribute('theme', theme);
+
+  if (label) {
+    scriptEl.setAttribute('label', label);
+  }
+
+  return scriptEl;
+};
+
+const putChildElement = <T extends HTMLElement, U extends HTMLElement>(parent: T, child: U): T => {
+  parent.childNodes.forEach(node => node.remove());
+
+  parent.appendChild(child);
+  return parent;
+};
 
 const Utterances = () => {
   const [colorMode] = useColorMode();
-  const utterancesTheme = colorMode === 'light' ? 'github-light' : 'photon-dark';
+  const utterancesTheme = colorMode === 'light' ? 'github-light' : 'github-dark';
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const scriptEl = createScriptElement({
+      repo: 'cmrfrd/taoa',
+      theme: utterancesTheme,
+      issueTerm: 'title'
+    });
+
+    putChildElement(ref.current, scriptEl);
+  }, [colorMode]);
 
   return (
-    <UtterancesContainer
-      ref={element => {
-        if (!element) {
-          return;
-        }
-
-        const scriptElement = document.createElement('script');
-        scriptElement.setAttribute('src', 'https://utteranc.es/client.js');
-        scriptElement.setAttribute('repo', 'cmrfrd/taoa');
-        scriptElement.setAttribute('issue-term', 'title');
-        scriptElement.setAttribute('theme', utterancesTheme);
-        scriptElement.setAttribute('crossorigin', 'anonymous');
-        scriptElement.setAttribute('async', 'true');
-        element.replaceChildren(scriptElement);
-      }}
-    />
+    <UtterancesContainer>
+      <div ref={ref} />
+    </UtterancesContainer>
   );
 };
 
