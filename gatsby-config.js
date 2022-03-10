f2536f54b5308f7fa37c2d5e08490f964939e71e
cmrfrd <alexanderjcomerford@gmail.com>
2022-03-10 10:55:33 -0500

    fix: update csp...again

--- gatsby-config.js
+++ gatsby-config.js
@@ -281,7 +281,7 @@ module.exports = {
           'object-src': 'none',
           'connect-src': "'self' https://www.google-analytics.com www.google-analytics.com",
           'script-src':
-            "'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com www.google-analytics.com taoa.us14.list-manage.com",
+            "'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com www.google-analytics.com taoa.us14.list-manage.com https://utteranc.es utteranc.es",
           'frame-src': "'self' https://utteranc.es",
           'style-src': "'self' 'unsafe-inline'",
           'img-src': "'self' data: www.google-analytics.com"
