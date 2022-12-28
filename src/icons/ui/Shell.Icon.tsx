75bb3a7c77cf0aa9618c88d1b226558465202f19
cmrfrd <alex@taoa.io>
2022-12-28 01:29:36 -0500

    fill shell icon with inv fill color

--- src/icons/ui/Shell.Icon.tsx
+++ src/icons/ui/Shell.Icon.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@types';
 
 import React from 'react';
 
-const ShellIcon: React.FC<Icon> = ({ fill }: Icon) => (
+const ShellIcon: React.FC<Icon> = ({ fill, invFill }: Icon) => (
   <svg height="24" version="1.1" width="24">
     <g transform="translate(0 -1028.4)">
       <path
@@ -10,10 +10,10 @@ const ShellIcon: React.FC<Icon> = ({ fill }: Icon) => (
         fill={fill}
       />
       <path d="m3 1049.4c-1.1046 0-2-0.9-2-2v-7-2-3h22v3 2 7c0 1.1-0.895 2-2 2h-9-9z" fill={fill} />
-      <path d="m4 1032.9v1.1l2 2.4-2 2.3v1.1l3-3.4-3-3.5z" fill="#ecf0f1" />
+      <path d="m4 1032.9v1.1l2 2.4-2 2.3v1.1l3-3.4-3-3.5z" fill={invFill} />
       <path
         d="m4 5.125v1.125l3 1.75-3 1.75v1.125l5-2.875-5-2.875zm5 4.875v1h5v-1h-5z"
-        fill="#ecf0f1"
+        fill={invFill}
         transform="translate(0 1028.4)"
       />
     </g>
